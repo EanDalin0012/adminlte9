3ceac33aeba077043cb030da7d2753ac29a03734
EanDalin0012 <endalined@gmail.com>
2020-05-01 18:40:51 +0700

    create component layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogingComponent } from './loging/loging.component';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { LayoutBlankComponent } from './layout/layout-blank/layout-blank.component';
+import { MLayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LayoutComponent,
+    LayoutBlankComponent,
     LogingComponent,
   ],
   imports: [
     BrowserModule,
+    MLayoutModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout/layout.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { LayoutBlankComponent } from './layout-blank/layout-blank.component';
+import { SettingsComponent } from './settings/settings.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    HeaderComponent,
+    LayoutBlankComponent,
+    SettingsComponent,
+    SideNavComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MLayoutModule { }
